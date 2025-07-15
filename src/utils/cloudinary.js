7fdb5faba3541894ffcc554a35465d41e3df6cb0
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-15 20:31:53 +0530

    .

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+export const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(
