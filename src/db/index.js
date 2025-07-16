650ff3f357cdb7e0bceb76ab5880d8e87cdb6de4
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-16 16:18:59 +0530

    toggle database connection

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,14 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-      // `${process.env.MONGODB_URI_OFFLINE}/projectDB`
-    );
-    console.log(
-      "MongoDB connected successfully to",
-      connectionInstance.connection.host
-    );
+    // const connectionInstance = await mongoose.connect(
+    //   `${process.env.MONGODB_URI}/${DB_NAME}`
+    //   // `${process.env.MONGODB_URI_OFFLINE}/projectDB`
+    // );
+    // console.log(
+    //   "MongoDB connected successfully to",
+    //   connectionInstance.connection.host
+    // );
   } catch (error) {
     console.error("Error in connectDb: MongoDB connection FAILED:", error);
     process.exit(1);
