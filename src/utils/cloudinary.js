cb225b9bf5242deff5927532a7fea48eaeca60e6
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-20 18:48:38 +0530

    fix: Clodinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,24 +1,28 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+import ApiError from "./ApiError.js";
 
 const uploadOnCloudinary = async (localFilePath) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
   try {
-    if (!localFilePath) return null;
+    if (!localFilePath) {
+      throw new Error(400, "Local file path is required for upload");
+    }
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("File is uploaded on cloudinary", response.url);
+    // Removing the local file after upload
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     // Removing locally saved file if upload fails
     fs.unlinkSync(localFilePath);
-    return null;
+    return new ApiError(500, "Cloudinary upload failed");
   }
 };
 
