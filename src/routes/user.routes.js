037709e961b1809e441cb7b9800bac19c677e64c
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-21 21:00:10 +0530

    add route /login and /logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 // import uploadOnCloudinary from "../utils/cloudinary.js";
 
 const router = Router();
@@ -12,5 +13,11 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+// Secure routes
+
+router.route("/logout").post(verifyJwt, logoutUser);
 
 export default router;
