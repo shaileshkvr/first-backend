5213ec562a7c6f8d0724d049ca2656b73bea3365
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-21 20:58:46 +0530

    add auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import ApiError from "../utils/ApiError";
+import asyncHandler from "../utils/asyncHandler";
+
+// _ is used to ignore the unused parameter 'res' in the function
+
+export const verifyJwt = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+
+    const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodeToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
