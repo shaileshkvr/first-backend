641ed3ab06ddb5617c406461ad49a4be5a8b0274
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-07-28 19:44:59 +0530

    feat(subscription): add validation to prevent self-subscription and ensure subscriber field is required

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -10,10 +10,18 @@ const SubscriptionSchema = new mongoose.Schema(
     subscriber: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
   },
   { timestamps: true }
 );
+
+SubscriptionSchema.pre("save", function (next) {
+  if (this.channel.toString() === this.subscriber.toString()) {
+    return next(new Error("User cannot subscribe to themselves."));
+  }
+  next();
+});
 
 const Subscription = mongoose.model("Subscription", SubscriptionSchema);
 
